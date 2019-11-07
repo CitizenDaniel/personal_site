2f4a8d425b3e70e50a0c7aa8d24815d857e32807
Dan <282802+CitizenDaniel@users.noreply.github.com>
2019-11-07 11:42:18 -0800

    Revert "bugfix 1"
    This reverts commit 36eba4d30f731fb338fca6d95a0545fbabbe0e67.

--- src/sections/PerformanceSection.jsx
+++ src/sections/PerformanceSection.jsx
@@ -88,57 +88,55 @@ const performanceSection = () => {
   return (
     <sectionContainer id="performance">
       <div css={styles.sectionGrid}>
-        
-        <sectionHeader text="Reading" colorNumber="1" />
+        <div>
+          <sectionHeader text="Reading" colorNumber="1" />
 
-        <div css={styles.grid}>
-          {readings.map(performance => (
-            <whiteBox key={performance.name} css={styles.whiteBox}>
-              <a
-                href={performance.webpageURL}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={performance.imgPath}
-                  css={styles.itemImage}
-                  alt={performance.name}
-                />
-              </a>
-
-              <a
-                href={performance.webpageURL}
-                target="_blank"
-                rel="noopener noreferrer"
-                css={styles.itemTitle}
-              >
-                {performance.name}
-                <iconExtLink />
-              </a>
-
-              <p css={styles.description}>{performance.description}</p>
-
-
-              {performance.blogPath && (
-                <link
-                  to={performance.blogPath}
+          <div css={styles.grid}>
+            {readings.map(performance => (
+              <whiteBox key={performance.name} css={styles.whiteBox}>
+                <a
+                  href={performance.webpageURL}
                   target="_blank"
-                  css={styles.externalLink}
+                  rel="noopener noreferrer"
                 >
-                  関連ブログ
-                </link>
-              )}
+                  <img
+                    src={performance.imgPath}
+                    css={styles.itemImage}
+                    alt={performance.name}
+                  />
+                </a>
 
-            </whiteBox>
-          ))}
+                <a
+                  href={performance.webpageURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  css={styles.itemTitle}
+                >
+                  {performance.name}
+                  <iconExtLink />
+                </a>
+
+                <p css={styles.description}>{performance.description}</p>
+
+
+                {performance.blogPath && (
+                  <link
+                    to={performance.blogPath}
+                    target="_blank"
+                    css={styles.externalLink}
+                  >
+                    関連ブログ
+                  </link>
+                )}
+
+              </whiteBox>
+            ))}
+          </div>
         </div>
-      </div>
-      <div css={styles.sectionGrid}>
+        <div>
+          <sectionHeader text="Listening to" colorNumber="1" />
 
-        
-        <sectionHeader text="Listening to" colorNumber="1" />
-
-        <div css={styles.grid}>
+          <div css={styles.grid}>
             {music.map(performance => (
               <whiteBox key={performance.name} css={styles.whiteBox}>
                 <a
@@ -178,6 +176,7 @@ const performanceSection = () => {
 
               </whiteBox>
             ))}
+          </div>
         </div>
       </div>
     </sectionContainer>
