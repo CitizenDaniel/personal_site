57a83b6f258e6822f534cf75a64729d5127d9b03
Dan <282802+CitizenDaniel@users.noreply.github.com>
2019-11-08 16:22:59 -0800

    tidying code

--- src/sections/ProfileSection.jsx
+++ src/sections/ProfileSection.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import AgeCalculator from '../components/AgeCalculator';
-import Hr from '../components/Hr';
-import IconExtLink from '../components/IconExtLink';
-import SectionContainer from '../components/SectionContainer';
-import SectionHeader from '../components/SectionHeader';
-import WhiteBox from '../components/WhiteBox';
+import react from 'react';
+import sectionContainer from '../components/SectionContainer';
+import sectionHeader from '../components/SectionHeader';
+import whiteBox from '../components/WhiteBox';
 
 import dredmor0 from '../images/dredmor0.png'
 import ce from "../images/ce1.png"
@@ -14,26 +11,26 @@ import sc0 from "../images/sc0.png"
 
 import { rhythm } from '../utils/typography';
 
-const ProfileSection = () => {
+const profileSection = () => {
 
   const projects = [
     {
       name: 'Dungeons of Dredmor',
       description:
-        'A dungeon game with lutefisk. This project was the foundation for the game studio Gaslamp Games, worked on in basements and couches on evenings and weekends, while everyone in the team was busy with their full-time job or school.',
-      webpageURL: 'https://dungeonsofdredmor.com/',
-      imgPath: dredmor0,
+        "A dungeon game with lutefisk. This project was the foundation for the game studio Gaslamp Games, worked on in basements and couches on evenings and weekends, while everyone in the team was busy with their full-time job or school.",
+      webpageURL: "https://dungeonsofdredmor.com/",
+      imgPath: dredmor0
     },
     {
-      name: 'Clockwork Empires',
-      description: 'A game about the perils of the unknown, The Sims crossed with Settlers, and H.P. Lovecraft. This project was a labor of love, but was ultimately overly ambitious and suffered from scope creep and unwieldy systems.',
-      imgPath: ce,
+      name: "Clockwork Empires",
+      description: "A game about the perils of the unknown, The Sims crossed with Settlers, and H.P. Lovecraft. This project was a labor of love, but was ultimately overly ambitious and suffered from scope creep and unwieldy systems.",
+      imgPath: ce
     },
     {
-      name: 'Digital Self-Service',
+      name: "Digital Self-Service",
       description: "My first \"startup\", Finn AI\'s Digital Self-Service assistant helps users with common banking problems that can\'t be resolved easily via mobile or web. The product saved end users time, and both reduced operational costs and built trust for financial institutions.",
-      webpageURL: 'https://www.finn.ai',
-      imgPath: finn0,
+      webpageURL: "https://www.finn.ai",
+      imgPath: finn0
     },
     {
       name: "Untitled Space Game",
@@ -44,52 +41,52 @@ const ProfileSection = () => {
 
   const styles = {
     grid: {
-      display: 'grid',
+      display: "grid",
       gridGap: rhythm(2),
-      gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 350px))',
-      justifyContent: 'center',
-      margin: 'auto',
-      maxWidth: 1200,
+      gridTemplateColumns: "repeat(auto-fit, minmax(300px, 350px))",
+      justifyContent: "center",
+      margin: "auto",
+      maxWidth: 1200
     },
     whiteBox: {
-      padding: rhythm(1),
+      padding: rhythm(1)
     },
     itemImage: {
-      border: '1px solid #aaa',
-      objectFit: 'cover',
-      height: '160px',
-      width: '100%',
-      filter: 'brightness(95%) grayscale(30%)',
+      border: "1px solid #aaa",
+      objectFit: "cover",
+      height: "160px",
+      width: "100%",
+      filter: "brightness(95%) grayscale(30%)"
     },
     itemTitle: {
-      color: '#262626',
-      display: 'block',
-      fontWeight: 'bold',
-      margin: '1rem 0',
-      textDecoration: 'none',
+      color: "#262626",
+      display: "block",
+      fontWeight: "bold",
+      margin: "1rem 0",
+      textDecoration: "none"
     },
     description: {
-      fontSize: '0.8rem',
-      marginBottom: '0.8rem',
+      fontSize: "0.8rem",
+      marginBottom: "0.8rem"
     },
     externalLink: {
-      color: '#999',
-      fontSize: '0.8rem',
-      textDecoration: 'none',
-      marginRight: '1rem',
+      color: "#999",
+      fontSize: "0.8rem",
+      textDecoration: "none",
+      marginRight: "1rem",
       ':hover': {
-        background: '#eee',
-      },
-    },
+        background: "#eee"
+      }
+    }
 };
 
   return (
-    <SectionContainer id="profile" colorNumber="2" skew skewReverse>
-      <SectionHeader text="Things I've made" colorNumber="2" />
+    <sectionContainer id="profile" colorNumber="2" skew skewReverse>
+      <sectionHeader text="Things I've made" colorNumber="2" />
 
       <div css={styles.grid}>
         {projects.map(project => (
-          <WhiteBox key={project.name} css={styles.whiteBox}>
+          <whiteBox key={project.name} css={styles.whiteBox}>
             <a
               href={project.webpageURL}
               target="_blank"
@@ -116,21 +113,21 @@ const ProfileSection = () => {
 
 
             {project.blogPath && (
-              <Link
+              <window.Link
                 to={project.blogPath}
                 target="_blank"
                 css={styles.externalLink}
               >
                 関連ブログ
-              </Link>
+              </window.Link>
             )}
 
-          </WhiteBox>
+          </whiteBox>
         ))}
       </div>
 
-    </SectionContainer>
+    </sectionContainer>
   );
 };
 
-export default ProfileSection;
+export default profileSection;
