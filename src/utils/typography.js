a393a5a6d08b1d3f14260d065ef2d6f8f96d9f63
Dan Jacobsen <dan@danjacobsen.com>
2021-02-17 11:59:13 -0800

    Cleanup - Removed JP font reference that was confusing translator extensions

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,26 +3,7 @@ import Typography from 'typography';
 const typography = new Typography({
   bodyColor: '#262626',
   baseFontSize: '16px',
-  bodyFontFamily: [
-    'Noto Sans JP',
-    // refer: https://munyagu.com/2094/
-    '游ゴシック Medium', // win
-    'Yu Gothic Medium', // win
-    '游ゴシック体', // mac
-    'YuGothic', // mac
-    'メイリオ', // win 7
-    'sans-serif',
-  ],
   headerColor: '#262626',
-  headerFontFamily: [
-    'Noto Sans JP',
-    '游ゴシック Medium', // win
-    'Yu Gothic Medium', // win
-    '游ゴシック体', // mac
-    'YuGothic', // mac
-    'メイリオ', // win 7
-    'sans-serif',
-  ],
   overrideStyles: () => ({
     'ul, li': {
       listStyle: 'none',
